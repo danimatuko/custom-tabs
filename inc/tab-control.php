e706e72bce9742fef89e7830ef2ecf2783a66cd2
Dani Matuko <danimatuko@pop-os.lan>
2024-07-06 21:18:39 +0300

    add style for mobile the static tab and add img assets

--- inc/tab-control.php
+++ inc/tab-control.php
@@ -7,62 +7,59 @@ function custom_tabs_shortcode($atts, $content = null) {
     // Output the HTML structure
     ob_start();
 ?>
-<div class="tabs-container">
+    <div class="tabs-container">
 
-    <div class="tabs">
-        <!-- Static First Tab -->
-        <div class="tab">
-            <button class="tablinks active" onclick="openTab(event, 'staticTab')">Static Tab</button>
-        </div>
+        <div class="tabs">
+            <!-- Static First Tab -->
+            <div class="tab">
+                <button class="tablinks active" onclick="openTab(event, 'staticTab')">Static Tab</button>
+            </div>
 
-        <!-- Dynamically Generated Tabs -->
-        <?php
+            <!-- Dynamically Generated Tabs -->
+            <?php
             for ($i = 1; $i <= 6; $i++) :
                 if (!empty($options['tab' . $i . '_title']) && !empty($options['tab' . $i . '_content'])) :
             ?>
-        <div class="tab">
-            <button class="tablinks" onclick="openTab(event, 'tab<?php echo $i; ?>')">
-                <?php echo esc_html($options['tab' . $i . '_title']); ?>
-            </button>
-        </div>
-        <?php
+                    <div class="tab">
+                        <button class="tablinks" onclick="openTab(event, 'tab<?php echo $i; ?>')">
+                            <?php echo esc_html($options['tab' . $i . '_title']); ?>
+                        </button>
+                    </div>
+            <?php
                 endif;
             endfor;
             ?>
-    </div>
+        </div>
 
-    <div class="div">
-        <!-- Static Tab Content -->
-        <div id="staticTab" class="tabcontent active">
-            <h3>Static Tab Content</h3>
-            <?php include plugin_dir_path(__FILE__) . '../partials/static-tab-content.php'; 
+        <div class="div">
+            <!-- Static Tab Content -->
+            <div id="staticTab" class="tabcontent active">
+                <?php include plugin_dir_path(__FILE__) . '../partials/static-tab-content.php';
                 ?>
-        </div>
+            </div>
 
-        <!-- Dynamically Generated Tab Content -->
-        <?php
+            <!-- Dynamically Generated Tab Content -->
+            <?php
             for ($i = 1; $i <= 6; $i++) :
                 if (!empty($options['tab' . $i . '_title']) && !empty($options['tab' . $i . '_content'])) :
             ?>
-        <div id="tab<?php echo $i; ?>" class="tabcontent">
-            <h3><?php echo esc_html($options['tab' . $i . '_title']); ?></h3>
-            <p><?php echo wp_kses_post($options['tab' . $i . '_content']); ?></p>
-        </div>
-        <?php
+                    <div id="tab<?php echo $i; ?>" class="tabcontent">
+                        <h3><?php echo esc_html($options['tab' . $i . '_title']); ?></h3>
+                        <p><?php echo wp_kses_post($options['tab' . $i . '_content']); ?></p>
+                    </div>
+            <?php
                 endif;
             endfor;
             ?>
+        </div>
+
+        <picture class="logos">
+            <source srcset="<?php echo plugin_dir_url(__FILE__) . '../src/img/logos-large.png'; ?>" media="(min-width: 800px)" style="width:947px">
+            <source srcset="<?php echo plugin_dir_url(__FILE__) . '../src/img/logos-small.png'; ?>" media="(max-width: 799px)">
+            <img src="<?php echo plugin_dir_url(__FILE__) . '../src/img/logos-small.png'; ?>" alt="Logo">
+        </picture>
+
     </div>
-
-    <picture class="logos">
-        <source srcset="<?php echo plugin_dir_url(__FILE__) . '../src/img/logos-large.png'; ?>"
-            media="(min-width: 800px)" style="width:947px">
-        <source srcset="<?php echo plugin_dir_url(__FILE__) . '../src/img/logos-small.png'; ?>"
-            media="(max-width: 799px)">
-        <img src="<?php echo plugin_dir_url(__FILE__) . '../src/img/logos-small.png'; ?>" alt="Logo">
-    </picture>
-
-</div>
 <?php
     return ob_get_clean();
 }
--- partials/static-tab-content.php
+++ partials/static-tab-content.php
@@ -1,1 +1,38 @@
-This is the static content of the first tab.
+<div class="grid grid-wrapper">
+    <div class="box box-light box-main">
+        <img class="quotation-mark"
+            src="<?php echo plugin_dir_url(dirname(__FILE__)) . 'src/img/quotation-mark.png'; ?>">
+        <q>
+            <strong>Riskified has enabled safe, fast, and seamless payments </strong> throughout our collaboration.
+            We’re
+            excited
+            to see what opportunities we can unlock in the future.
+        </q>
+        <div class="customer">
+            <img src="<?php echo plugin_dir_url(dirname(__FILE__)) . 'src/img/Michael-Fleisher.png'; ?>">
+            <div>
+                <div class="name">Michael Fleisher</div>
+                <div class="role">Chief Financial Officer</div>
+            </div>
+        </div>
+        <div class="company">
+            <img src="<?php echo plugin_dir_url(dirname(__FILE__)) . 'src/img/Merchant-logos.png'; ?>">
+
+        </div>
+    </div>
+    <div>
+        <div class="grid box-pair">
+            <div class="box box-light">
+                <strong>50%</strong>
+                <p>Reduction in the cost of fraud</p>
+
+            </div>
+            <div class="box box-dark">
+                <img class="arrow" src="<?php echo plugin_dir_url(dirname(__FILE__)) . 'src/img/arrow.png'; ?>">
+                <strong>
+                    Read Wayfair case study
+                </strong>
+            </div>
+        </div>
+    </div>
+</div>
